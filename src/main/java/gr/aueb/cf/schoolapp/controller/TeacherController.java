3ed0d0cb390e3c4429ad359a7f12e000deb4c0b4
a8anassis <a8anassis@gmail.com>
2025-07-15 20:57:02 +0300

    feat: add Teacher Controller

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -1,29 +1,67 @@
 package gr.aueb.cf.schoolapp.controller;
 
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
+import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.repository.RegionRepository;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/school/teachers")
+@RequiredArgsConstructor
 public class TeacherController {
 
+    private final ITeacherService teacherService;
     private final RegionRepository regionRepository;
+    private final Mapper mapper;
 
-    @Autowired
-    public TeacherController(RegionRepository regionRepository) {
-        this.regionRepository = regionRepository;
-    }
+//    @Autowired
+//    public TeacherController(RegionRepository regionRepository) {
+//        this.regionRepository = regionRepository;
+//    }
 
     @GetMapping("/insert")
     public String getTeacherForm(Model model) {
         model.addAttribute("teacherInsertDTO", new TeacherInsertDTO());
         model.addAttribute("regions", regionRepository.findAll(Sort.by("name"))); // PagingAndSortingRepository
         return "teacher-form";
+    }
+
+    public String saveTeacher(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
+                              BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+
+        Teacher savedTeacher;
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("regions", regionRepository.findAll(Sort.by("name")));
+            return "teacher-form";
+        }
+
+        try {
+            savedTeacher = teacherService.saveTeacher(teacherInsertDTO);
+            TeacherReadOnlyDTO readOnlyDTO = mapper.mapToTeacherReadOnlyDTO(savedTeacher);
+            redirectAttributes.addFlashAttribute("teacher", readOnlyDTO);
+            // The Post-Redirect-Get (PRG) pattern είναι ένα web development pattern
+            // το οποίο εμποδίζει τα duplicate submissions κάνοντας redirect μετά από ένα POST
+            return "redirect:/school/teachers";
+        } catch (EntityAlreadyExistsException | EntityInvalidArgumentException e) {
+            model.addAttribute("regions", regionRepository.findAll(Sort.by("name")));
+            model.addAttribute("errorMessage", e.getMessage());
+            return "teacher-form";
+        }
     }
 }

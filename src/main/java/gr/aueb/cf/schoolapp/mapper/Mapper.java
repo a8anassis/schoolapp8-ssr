9bfa5109f6266fd1d741ca3595cdd1b396d479d4
a8anassis <a8anassis@gmail.com>
2025-07-15 20:57:11 +0300

    feat: add mapper

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.schoolapp.mapper;
 
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,10 @@ public class Mapper {
 
     public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
         return new Teacher(null, null, dto.getVat(), dto.getFirstname(), dto.getLastname(), null);
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(), teacher.getUpdatedAt(),
+                teacher.getFirstname(), teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
     }
 }

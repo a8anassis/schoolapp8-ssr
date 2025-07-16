bc4079783cda68308fb9db53e4a9275088bb1df5
a8anassis <a8anassis@gmail.com>
2025-07-16 20:36:17 +0300

    fix: add to teacher edit dto

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf.schoolapp.mapper;
 
+import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
@@ -15,5 +16,10 @@ public class Mapper {
     public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
         return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(), teacher.getUpdatedAt(), teacher.getUuid(),
                 teacher.getFirstname(), teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
+    }
+
+    public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
+        return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
+                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
     }
 }

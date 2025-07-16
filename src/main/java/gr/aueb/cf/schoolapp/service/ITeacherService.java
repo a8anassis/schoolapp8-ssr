2e08c4efc8078da264455e4c6d3b485b3c06dc49
a8anassis <a8anassis@gmail.com>
2025-07-16 19:13:51 +0300

    feat: add pagination for teachers

--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -3,9 +3,12 @@ package gr.aueb.cf.schoolapp.service;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import org.springframework.data.domain.Page;
 
 public interface ITeacherService {
     Teacher saveTeacher(TeacherInsertDTO teacherInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(int page, int size);
 }

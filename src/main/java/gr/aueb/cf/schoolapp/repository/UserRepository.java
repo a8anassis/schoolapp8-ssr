1f078e324d56e7fcbb817f3dcd135ec41f4ef917
a8anassis <a8anassis@gmail.com>
2025-07-22 20:39:48 +0300

    feat: add user repository

---
+++ src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.schoolapp.repository;
+
+import gr.aueb.cf.schoolapp.core.enums.Role;
+import gr.aueb.cf.schoolapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByRole(Role role);
+    Optional<User> findByUsername(String username);
+}

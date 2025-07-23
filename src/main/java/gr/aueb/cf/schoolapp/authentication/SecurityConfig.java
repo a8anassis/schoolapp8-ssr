44d433a881e355805d9900004aff6b2e6e33b8fa
a8anassis <a8anassis@gmail.com>
2025-07-23 18:52:12 +0300

    feat: add security config

---
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/SecurityConfig.java
@@ -0,0 +1,48 @@
+package gr.aueb.cf.schoolapp.authentication;
+
+import gr.aueb.cf.schoolapp.core.enums.Role;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "index.html").permitAll()
+                        .requestMatchers("/school/users/register").permitAll()
+                        .requestMatchers("/school/teachers/**").hasAnyAuthority(Role.TEACHER.name())
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+//                        .usernameParameter("username")
+//                        .passwordParameter("parameter")
+//                        .loginProcessingUrl("/login")
+//                        .failureUrl("/login?error")
+                        .defaultSuccessUrl("/school/teachers", true)
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+                return http.build();
+    }
+}
